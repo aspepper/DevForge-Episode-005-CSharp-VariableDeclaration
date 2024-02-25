72abdc36babdd397d9a905dc03737f1531415333
Alex Pimenta <aspepper@hotmail.com>
2024-02-25 15:43:45 -0300

    first commit

---
+++ Episode5-Simple-Sample/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Declaration and assignment of variables
+        int age = 51;
+        double height = 1.70;
+        string name = "Alex Pimenta";
+        bool isStudent = true;
+        string student = isStudent ? "Yes" : "No";
+
+        // Displaying variable values
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Age: {age}");
+        Console.WriteLine($"Height: {height}");
+        Console.WriteLine($"Is Student? {student}");
+    }
+}
